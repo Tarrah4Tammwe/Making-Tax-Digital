9ea15658ff984d360d67d294c83e10b7c1dd7415
Tarrah Nhari <tarrahnhari@Tarrahs-MacBook-Pro.local>
2026-05-07 17:48:27 +0100

    Restore @ alias with proper jsconfig.json

--- app/how-much-cost/page.js
+++ app/how-much-cost/page.js
@@ -1,5 +1,5 @@
-import AnswerPageLayout from '../../components/AnswerPageLayout.js'
-import Calculator from '../../components/Calculator.js'
+import AnswerPageLayout from '@/components/AnswerPageLayout.js'
+import Calculator from '@/components/Calculator.js'
 import Link from 'next/link'
 
 export const metadata = {
